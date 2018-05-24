9c6dd26bc484b06bad98a588c4d4a64f9231d883
Anthony Griggs <Anthony.Griggs1@marist.edu>
2018-05-24 13:43:31 -0500

    Implemented input, although the game does not recognize any commands yet. Allows the player to continue the narrative by simply pressing enter by using getline (They do not need to input anything). This will also allow for two word commands, which the python version uses.

--- main.cpp
+++ main.cpp
@@ -12,18 +12,23 @@ using namespace std; //used so we don't have to type std::cout
 
 //Define our locations as strings for now
 string nowhere = "You find yourself in an empty, white space. A void. How? Why? Where? Who? Many thoughts race through your head, with no definitive answer manifesting...";
-string office = "You now find yourself in an office. Elegant. Wait. WAIT. What to every, erm, nothing? You were just in the middle of nowhere? This isn't right!";
+string office = "You now find yourself in an office. Elegant. Wait. WAIT. What happened to every, erm, nothing? You were just in the middle of nowhere! This isn't right!";
 string forest = "The office disappears, and turns into a forest. You begin to figure it out: you must be dreaming! You breathe a sigh of relief. Soon this wierd dream will all be over, you'll wake up, and return to normal life.";
 string bed = "You are in your bed. Finally! It's over.";
 
 string cont = "<Press enter to ";
 int main() {
     // << is concatenation
+    string dummy;
     cout << nowhere << endl << cont << "continue>" << endl; //endl--endline--Would make more sense if it was endln, but whatever
     //Use cin to give the player control?
-    cin; //Doesn't work. Doing cin >> <string> requires the user to input a character before hitting enter, and that's not what I want.
+  //  cin; //Doesn't work. Doing cin >> <string> requires the user to input a character before hitting enter, and that's not what I want.
+    getline(cin, dummy); //This seems to work, and will be better to use
     cout << office << endl << cont << "continue>" << endl;
+    getline(cin, dummy);
     cout << forest << endl << cont << "continue>" << endl;
+    getline(cin, dummy);
     cout << bed << endl << cont << "continue>" << endl;
+    getline(cin, dummy);
     return 0;
 }
