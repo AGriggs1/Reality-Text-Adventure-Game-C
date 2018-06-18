fb86fefef77170f5d8f3e51409206f3502c2728f
Anthony Griggs <Anthony.Griggs1@marist.edu>
2018-06-17 19:23:54 -0500

    Added null constructor for initializing arrays.

--- Locale.h
+++ Locale.h
@@ -3,7 +3,8 @@
 #include <iostream>
 #include <string>
 
-
+//TODO: Implement logic for items
+//TODO: write out reset function when the time comes
 class Locale {
     //private instance variables
     int _ID;
@@ -15,10 +16,22 @@ class Locale {
     std::string DESC_SHORT;
     std::string DESC_EXAMINE;
     public:
+        //public instance variables
+        std::string _longDescription;
+        std::string _shortDescription;
+        std::string _examineDescription;
+        //Null constructor
+        Locale() {
+            _ID = -1;
+            _longDescription = "This location has not been initiated, is this therefore nil. For real this time.";
+            _shortDescription = "nil";
+            _visited = false;
+            //Don't worry about copying, as this should never actully be used once it's complete
+        }
 
+        //Constructor
         Locale(int ID, std::string items[], std::string longDescription, std::string shortDescription, std::string examineDescription) {
             _ID = ID;
-          //  _items = items; Not even allowed to create a reference. Cool. Nice. Easy. Intuitive.
             _longDescription = longDescription;
             _shortDescription = shortDescription;
             _examineDescription = examineDescription;
@@ -26,18 +39,12 @@ class Locale {
             DESC_LONG = longDescription;
             DESC_SHORT = shortDescription;
             DESC_EXAMINE = examineDescription;
-            //so getting the size of the array through something like arr.size() doesn't work, despite my IDE listing it. What is wrong with this language?
-            //So if I wanted the size of the array, I'd also have to pass its length in the constructor, but nah.
             //Doing arr1 = arr2 usually results in arr2 simply referencing arr1 in most languages, and my intuition tells me C++ is the same
-            for(int i = 0; i < 100; i++) {
+            for (int i = 0; i < 100; i++) {
                 _items[i] = items[i];
                 ITEMS[i] = items[i];
             }
         }
-        //public instance variables
-        std::string _longDescription;
-        std::string _shortDescription;
-        std::string _examineDescription;
         //getters
         std::string getLocationDescription() {
             if(_visited) return _shortDescription;
