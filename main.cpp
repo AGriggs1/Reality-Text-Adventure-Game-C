e49255197f98771c435a908e63b7f5a85de8dcaa
Anthony Griggs <Anthony.Griggs1@marist.edu>
2018-06-26 18:42:20 -0500

    Added original first 5 locales, added the commands 'quit', 'help' and 'look' to work in tutorial.

--- main.cpp
+++ main.cpp
@@ -17,10 +17,30 @@ using namespace std; //used so we don't have to type std::cout
  * Define Locales
  */
 string longDesc = "You find yourself in an empty, white space. A void. How? Why? Where? Who? Many thoughts race through your head, with no definitive answer manifesting...";
-string shortDesc = "You are nowhere, it seems.";
 string examineDesc = "You see nothing of use.";
 string items[100];
 Locale voidDummy(0, items, longDesc, "NULL", "NULL");
+Locale voidC(1, items, "You are in the center circle. So. Where to first?", "You are standing on the center circle.", examineDesc);
+
+Locale voidN(2, items, "You follow the line North. Once you reach the circle, you notice it has the letter 'N' on it. "
+                       "The void transforms into a lush forest teeming with pine trees. You hear the wind breeze through the trees, "
+                       "the songs of birds and feel welcome.", "You return to the forest", examineDesc);
+
+Locale voidS(3, items, "You follow one of the lines to a circle that has the letter 'S' on it. "
+                       "The area around transforms. Now you at the edge of a cliff overlooking the open sea. You hear the waves crashing "
+                       "against the crags below, which resemble hungry teeth tearing into the vast watters, eager to do the same to you. You feel a sense of somberness.",
+                       "You return to the cliffside", examineDesc);
+
+Locale voidE(4, items, "You follow the line leading West, stopping at the circle with the letter 'W' on it. "
+                       "The area transforms into the ruins of city. Cars rusted, buildings overgrown with moss and vines. Nature has reclaimed what it once lost."
+                       " You can't help but feel curious about the fate of this place.", "You retun to the city ruins.", examineDesc);
+
+Locale voidW(5, items, "You head East, coming to a circle with the letter 'E' on it. The area transforms,"
+                       " and now you are in an office. Desks overflowing with paperwork and the inescapable stench of stale cofee makes you feel anxious.",
+                        "You return to the office", examineDesc);
+
+
+
 
 //Locale nowhere(0, items, longDesc, shortDesc, examineDesc);
 //Locale office(1, items, "You now find yourself in an office. Elegant. Wait. WAIT. What to happened to every, erm, nothing? You were just in the middle of nowhere! This isn't right!", "You're in an office.", examineDesc);
@@ -28,7 +48,7 @@ Locale voidDummy(0, items, longDesc, "NULL", "NULL");
 //Locale bed(3, items, "You are in your bed. Finally! It's over.", "Wakey-wakey!", examineDesc);
 
 //Create an array to act as a dictionary for the locales
-Locale locations[50] = {nowhere, office, forest, bed};
+Locale locations[50] = {voidDummy, voidC, voidN, voidS, voidE, voidW};
 
 string cont = "continue";
 /*
@@ -87,10 +107,38 @@ void examineLocation(Locale locale) {
  * GAMEPLAY FUNCTIONS
  *
  */
-void tutorial(Player love) {
+
+/*
+ * tutorial
+ * simulates the game on a much smaller scale to give the player a chance to learn the ropes
+ * returns true if the player quit
+ * return false if completed
+ */
+bool tutorial(Player love) {
     //TODO: tutorial
     bool completed = false;
-    //while(!completed){}
+    cout <<  "Suddenly a circle appears beneath your feet, with lines going in four directions, ultimately leading to four circles."
+             "\n\n<Type 'North', 'South', 'East', or 'West' to head in that direction. Type 'Help' to view all available commands.>" << endl;
+    while(!completed) {
+        string command;
+        cout << locations[love.getLocale()].getLocationDescription() << endl;
+        getline(cin, command);
+        //Check what the command was...
+        //TODO: compareIgnoreCase
+        if(command == "quit") return true;
+        else if(command == "help") cout << "List of commands:\n"
+                                           "Quit - ends the game\n"
+                                           "Help - shows help\n"
+                                           "North - moves north\n"
+                                           "South - moves south\n"
+                                           "East - moves east\n"
+                                           "West - moves west\n"
+                                           "Look - displays the long description of your current location\n";
+        else if(command == "look") cout << locations[love.getLocale()]._longDescription << endl;
+        else if(command == "I know what I am doing I know that not all control paths return a value will you just run and not assume me to be an idiot?") completed = true;
+        locations[love.getLocale()].updateVisited();
+    }
+    return false;
 }
 /*
  *
@@ -107,16 +155,20 @@ bool init() {
     prompt(cont);
     getline(cin, dummy);
     cout << "???: Hello? Heellllooo? Can you hear me?\n\nHuh? Where is that voice coming from? This doesn't feel right..." << endl;
-    prompt("acknowledge the disembodied voice>");
+    prompt("acknowledge the disembodied voice");
+    getline(cin, dummy);
     cout << "???: Good. You're awake. About time! I was getting sooooo bored of just watching you sleep!\n\nDoesn't feel right isn't enough to describe this. What is going on here??" << endl;
     prompt(cont);
-    cout << "???: You look confused! Do you remember your name? Ya know, that thing people call you? Yeah. That!\n\n<Enter your name>\n";
+    getline(cin, dummy);
+    cout << "???: You look confused! Do you remember your name? Ya know, that thing people call you? Yeah. That!\n<Enter your name>\n";
     cin >> dummy;
     //TODO: Fun with names ;)
     cout << "???: " << dummy << "? Let's see... gotcha. Your cognitive abilities don't seem to be compromised...  I sense that you want answers. I get it, but I must follow 'protocols.' "
-                                " Ethics. Bleeeeh. Boring, really, but hey, it's a living. Let's just test your sense of orientation. Then we'll talk." << endl;
-    prompt(cont);
+                                " Ethics and all that human rights junk. Bleeeeh. Boring, really, but hey, it's a living. Let's just test your sense of orientation. Then we'll talk." << endl;
     Player mag(dummy, 1);
+    prompt(cont);
+    getline(cin, dummy); //Why? I have no idea. It's just the only way I could get it to work
+
     //Begin tutorial
     tutorial(mag);
     return copyright(mag.getScore(), true);
